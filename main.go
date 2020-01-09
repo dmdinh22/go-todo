d2c3119a009c7ef103e61cf607123c14edb09737
David Dinh <dmdinh22@gmail.com>
2020-01-08 22:58:53 -0800

    set up orm

--- main.go
+++ main.go
@@ -2,18 +2,34 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
+var db *gorm.DB
 
-func main()  {
+func main() {
 	router := gin.Default()
 
-	v1 := router.Group("/api/v1/todos") {
+	v1 := router.Group("/api/v1/todos")
+	{
 		v1.POST("/", createTodo)
 		v1.GET("/", fetchAllTodo)
 		v1.GET("/:id", updateTodo)
 		v1.DELETE("/:id", deleteTodo)
 	}
 
-	router.Run()
+	router.Run(":5000")
+}
+
+func init() {
+	//open db connection
+	var err error
+	db, err = gorm.Open("mysql", "root:Password1@/demo?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic("failed to connect to database")
+	}
+
+	// migrate schema
+	db.AutoMigrate(&todoModel{})
 }
